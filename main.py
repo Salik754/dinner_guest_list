b3cdb7298ac0f7b8c2a94b34b63535b62827eb55
Salik754 <salikahmadn505@deltalearns.ca>
2026-03-06 13:59:10 -0800

    added the psuedocode
    yayy

---
+++ main.py
@@ -0,0 +1,35 @@
+# Create empty guest list
+
+# Function add_guest
+#     ask for guest name
+#     format name
+#     check if name is empty
+#     check if name already exists
+#     if valid
+#         add guest to list
+
+# Function modify_guest
+#     ask which guest to modify
+#     if guest exists
+#         ask for new name
+#         replace name
+
+# Function remove_guest
+#     ask which guest to remove
+#     if guest exists
+#         remove guest
+
+# Function sort_guests
+#     sort the guest list alphabetically
+
+# Function show_guest_count
+#     print number of guests
+
+# Function show_invitations
+#     for each guest
+#         print invitation message
+
+# Main program
+#     show menu
+#     ask for choice
+#     run correct function
